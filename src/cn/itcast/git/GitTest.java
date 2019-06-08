3240f774f56fe5985d92d41e10a1507245ba78e8
chenxiaobin <979141202@qq.com>
2019-06-08 19:45:28 +0800

    将idea中的项目提交到本地

---
+++ src/cn/itcast/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+    }
+}
